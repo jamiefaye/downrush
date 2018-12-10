e250e9e8f68f3e29c7e21a539513d40def0ef474
jamiefaye <jamie@fentonia.com>
2018-12-09 19:58:27 -0600

    Remove test breakpoint spots

--- xmlView/src/viewXML.js
+++ xmlView/src/viewXML.js
@@ -537,9 +537,6 @@ function plotKit13(track, reftrack, obj) {
 		let labName = '';
 		if (row.drumIndex) {
 			let rowInfo = kitList[row.drumIndex];
-			if (!rowInfo) {
-				let cat = 2;
-			}
 			labName = rowInfo.name;
 			if (labName != undefined) {
 				let labdiv = $("<div class='kitlab'/>");
@@ -624,9 +621,6 @@ function plotKit14(track, reftrack, song, obj) {
 
 		if (row.drumIndex) {
 			let rowInfo = kitList[row.drumIndex];
-			if (!rowInfo) {
-				let cat = 1;
-			}
 			labName = rowInfo.name;
 			if (rowInfo.channel) {
 				let chanNum = Number(rowInfo.channel);
